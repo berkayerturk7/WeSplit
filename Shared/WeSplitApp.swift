c1364d7e7e931ef76a38d17768b54cd7a44a8126
berkay <berkayert7@gmail.com>
2022-09-03 18:09:52 +0300

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Berkay Ertürk on 3.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
